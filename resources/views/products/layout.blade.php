b68162ef21563e2e12443d8a56c5d00f58bb9ac5
christianbongcales <djchristianbongcales@gmail.com>
2023-03-28 14:15:11 -0800

    first commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Products</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
+    rel="stylesheet">
+</head>
+<body>
+    <h1> Laravel Products </h1>
+<div class="container">
+    <br>
+    @yield('content')
+</div>
+
+</body>
+</html>
